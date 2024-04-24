098062378481de2738364600205c5289a5f0918f
Nathan Whitehead <nwhitehe@gmail.com>
2024-04-24 10:05:18 -0600

    Cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use ab_glyph::{Font, FontRef, Glyph, GlyphId, ScaleFont};
 use rquickjs::{
-    class::Trace, function::IntoJsFunc, Class, Context, Ctx, Error, Function, Runtime, Value,
-    loader::{
-        BuiltinLoader, BuiltinResolver, FileResolver, ModuleLoader, ScriptLoader,
-    },
+    class::Trace,
     context::EvalOptions,
+    function::IntoJsFunc,
+    loader::{BuiltinLoader, BuiltinResolver, FileResolver, ModuleLoader, ScriptLoader},
+    Class, Context, Ctx, Error, Function, Runtime, Value,
 };
-use tiny_skia::{FillRule, LineCap, Paint, PathBuilder, Pixmap, Rect, Stroke, PremultipliedColorU8, Transform};
-use ab_glyph::{FontRef, Font, Glyph, GlyphId, ScaleFont};
+use tiny_skia::{
+    FillRule, LineCap, Paint, PathBuilder, Pixmap, PremultipliedColorU8, Rect, Stroke, Transform,
+};
 
 #[derive(Trace)]
 #[rquickjs::class]
@@ -36,10 +38,17 @@ impl DrawContext {
     }
 
     #[qjs(rename = "measureText")]
-    pub fn measure_text(& mut self, txtch: u32, scale: f64, font: i32) -> std::vec::Vec<f64> {
-        let bravura_font: FontRef = FontRef::try_from_slice(include_bytes!("../fonts/Bravura.otf")).unwrap();
-        let garamond_font: FontRef = FontRef::try_from_slice(include_bytes!("../fonts/EBGaramond-VariableFont_wght.ttf")).unwrap();
-        let chosen_font = if font == 0 { &garamond_font } else { &bravura_font };
+    pub fn measure_text(&mut self, txtch: u32, scale: f64, font: i32) -> std::vec::Vec<f64> {
+        let bravura_font: FontRef =
+            FontRef::try_from_slice(include_bytes!("../fonts/Bravura.otf")).unwrap();
+        let garamond_font: FontRef =
+            FontRef::try_from_slice(include_bytes!("../fonts/EBGaramond-VariableFont_wght.ttf"))
+                .unwrap();
+        let chosen_font = if font == 0 {
+            &garamond_font
+        } else {
+            &bravura_font
+        };
         let scaled_font = chosen_font.as_scaled(chosen_font.pt_to_px_scale(scale as f32).unwrap());
         let ch = char::from_u32(txtch).unwrap();
         let glyph: GlyphId = chosen_font.glyph_id(ch);
@@ -49,16 +58,25 @@ impl DrawContext {
     }
 
     #[qjs(rename = "fillText")]
-    pub fn fill_text(& mut self, txtch: u32, x: f64, y: f64, scale: f64, font: i32) {
+    pub fn fill_text(&mut self, txtch: u32, x: f64, y: f64, scale: f64, font: i32) {
         // Get font and scale from self.font
         let stride = self.surface.width();
         let width = self.width as i32;
         let height = self.height as i32;
-        let bravura_font: FontRef = FontRef::try_from_slice(include_bytes!("../fonts/Bravura.otf")).unwrap();
-        let garamond_font: FontRef = FontRef::try_from_slice(include_bytes!("../fonts/EBGaramond-VariableFont_wght.ttf")).unwrap();
-        let chosen_font = if font == 0 { &garamond_font } else { &bravura_font };
+        let bravura_font: FontRef =
+            FontRef::try_from_slice(include_bytes!("../fonts/Bravura.otf")).unwrap();
+        let garamond_font: FontRef =
+            FontRef::try_from_slice(include_bytes!("../fonts/EBGaramond-VariableFont_wght.ttf"))
+                .unwrap();
+        let chosen_font = if font == 0 {
+            &garamond_font
+        } else {
+            &bravura_font
+        };
         let ch = char::from_u32(txtch).unwrap();
-        let glyph: Glyph = chosen_font.glyph_id(ch).with_scale(chosen_font.pt_to_px_scale(scale as f32).unwrap());
+        let glyph: Glyph = chosen_font
+            .glyph_id(ch)
+            .with_scale(chosen_font.pt_to_px_scale(scale as f32).unwrap());
         let pixels = self.surface.pixels_mut();
         if let Some(g) = chosen_font.outline_glyph(glyph) {
             g.draw(|xx, yy, c| {
@@ -67,43 +85,55 @@ impl DrawContext {
                 if xi >= 0 && xi < width && yi >= 0 && yi < height {
                     let offset: usize = (yi as u32 * stride + xi as u32).try_into().unwrap();
                     let i: u8 = (c * 255.0) as u8;
-                    pixels[offset] = PremultipliedColorU8::from_rgba(0, 0, 0, i).unwrap();    
+                    pixels[offset] = PremultipliedColorU8::from_rgba(0, 0, 0, i).unwrap();
                 }
             });
         }
     }
 
-    pub fn save(& mut self, filename: String) {
+    pub fn save(&mut self, filename: String) {
         self.surface.save_png(filename).unwrap();
     }
 
     #[qjs(rename = "beginPath")]
-    pub fn begin_path(& mut self) {
+    pub fn begin_path(&mut self) {
         assert!(!self.in_path);
         self.in_path = true;
         self.path = Some(PathBuilder::new());
     }
 
     #[qjs(rename = "moveTo")]
-    pub fn move_to(& mut self, x: f64, y: f64) {
+    pub fn move_to(&mut self, x: f64, y: f64) {
         assert!(self.in_path);
         assert!(self.path.is_some());
-        self.path.as_mut().expect("path must be created").move_to(x as f32, y as f32);
+        self.path
+            .as_mut()
+            .expect("path must be created")
+            .move_to(x as f32, y as f32);
     }
 
     #[qjs(rename = "lineTo")]
-    pub fn line_to(& mut self, x: f64, y: f64) {
+    pub fn line_to(&mut self, x: f64, y: f64) {
         assert!(self.in_path);
         assert!(self.path.is_some());
-        self.path.as_mut().expect("path must be created").line_to(x as f32, y as f32);
+        self.path
+            .as_mut()
+            .expect("path must be created")
+            .line_to(x as f32, y as f32);
     }
 
-    pub fn stroke(& mut self, width: f64) {
+    pub fn stroke(&mut self, width: f64) {
         assert!(self.in_path);
         assert!(self.path.is_some());
         self.in_path = false;
         // FIXME: I'm cloning the path, then removing it. How do I take ownership and drop it?
-        let final_path = self.path.as_mut().expect("path must be created").clone().finish().unwrap();
+        let final_path = self
+            .path
+            .as_mut()
+            .expect("path must be created")
+            .clone()
+            .finish()
+            .unwrap();
         self.path = None;
         let mut paint = Paint::default();
         paint.set_color_rgba8(0, 0, 0, 255);
@@ -111,24 +141,37 @@ impl DrawContext {
         let mut stroke = Stroke::default();
         stroke.width = width as f32;
         stroke.line_cap = LineCap::Round;
-        self.surface.stroke_path(&final_path, &paint, &stroke, Transform::identity(), None);
+        self.surface
+            .stroke_path(&final_path, &paint, &stroke, Transform::identity(), None);
     }
 
-    pub fn fill(& mut self) {
+    pub fn fill(&mut self) {
         assert!(self.in_path);
         assert!(self.path.is_some());
         self.in_path = false;
         // FIXME: I'm cloning the path, then removing it. How do I take ownership and drop it?
-        let final_path = self.path.as_mut().expect("path must be created").clone().finish().unwrap();
+        let final_path = self
+            .path
+            .as_mut()
+            .expect("path must be created")
+            .clone()
+            .finish()
+            .unwrap();
         self.path = None;
         let mut paint = Paint::default();
         paint.set_color_rgba8(0, 0, 0, 255);
         paint.anti_alias = true;
-        self.surface.fill_path(&final_path, &paint, FillRule::Winding, Transform::identity(), None);
+        self.surface.fill_path(
+            &final_path,
+            &paint,
+            FillRule::Winding,
+            Transform::identity(),
+            None,
+        );
     }
 
     #[qjs(rename = "fillRect")]
-    pub fn fill_rect(& mut self, x: f64, y: f64, width: f64, height: f64) {
+    pub fn fill_rect(&mut self, x: f64, y: f64, width: f64, height: f64) {
         let mut paint = Paint::default();
         paint.set_color_rgba8(0, 0, 0, 255);
         paint.anti_alias = true;
@@ -136,7 +179,7 @@ impl DrawContext {
             Rect::from_xywh(x as f32, y as f32, width as f32, height as f32).unwrap(),
             &paint,
             Transform::identity(),
-            None
+            None,
         );
     }
 }
@@ -184,19 +227,25 @@ fn main() {
         BuiltinResolver::default(),
         FileResolver::default().with_path("./"),
     );
-    let loader = (
-        BuiltinLoader::default(),
-        ScriptLoader::default(),
-    );
+    let loader = (BuiltinLoader::default(), ScriptLoader::default());
     let runtime = Runtime::new().unwrap();
     let ctx = Context::full(&runtime).unwrap();
     runtime.set_loader(resolver, loader);
     ctx.with(|ctx| {
         let global = ctx.globals();
-        global.set("arg".to_string(), args[args.len() - 1].clone()).unwrap();
+        global
+            .set("arg".to_string(), args[args.len() - 1].clone())
+            .unwrap();
         Class::<DrawContext>::define(&global).unwrap();
         register_function(ctx.clone(), "print", print);
-        match ctx.eval_file_with_options::<(), _>("src/test.js", EvalOptions { global: false, strict: true, backtrace_barrier: false }) {
+        match ctx.eval_file_with_options::<(), _>(
+            "src/test.js",
+            EvalOptions {
+                global: false,
+                strict: true,
+                backtrace_barrier: false,
+            },
+        ) {
             Err(Error::Exception) => println!("{}", format_exception(ctx.catch())),
             Err(e) => println!("Error! {:?}", e),
             Ok(_) => (),
