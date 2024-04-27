8c8beebdbede0696f9f6d237ec5e3a8a9b72d536
Nathan Whitehead <nwhitehe@gmail.com>
2024-04-26 19:35:01 -0600

    Tidy up.

--- test/vexflow_unit_tests.js
+++ test/vexflow_unit_tests.js
@@ -1,9 +1,6 @@
 import { VexFlowTests } from '../../vexflow/tests/vexflow_test_helpers.js';
 
 import * as _ from '../../vexflow/tests/index.js';
-//import * as _ from '../../vexflow/tests/vf_prefix_tests.js';
-//import * as __ from '../../vexflow/tests/accidental_tests.js';
-
 
 console.log(`Running ${VexFlowTests.tests.length} tests`);
 
